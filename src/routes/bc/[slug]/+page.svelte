eb01b7c768129c6bb3a35e844cf633750d0fc503
david <david@d17e.dev>
2024-11-09 13:45:22 +0100

    slug page fix

--- src/routes/bc/[slug]/+page.svelte
+++ src/routes/bc/[slug]/+page.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
-  import type {PageData} from './$types';
 
-  let {data}: { data: PageData } = $props();
 </script>
 
-<h1>{data.title}</h1>
-<div>{@html data.content}</div>
+<div>Hello!</div>
