c355d501123c3e52c0ace6794479df14ae985bb2
david <david@d17e.dev>
2024-11-15 14:56:31 +0100

    bc page copy styling

--- src/routes/bc/[slug]/+page.svelte
+++ src/routes/bc/[slug]/+page.svelte
@@ -7,10 +7,13 @@
   <h1 class="title page-title">{post.title}</h1>
   <p>
     <b>Hi there! :)</b><br/><br/>
-    Thank you for scanning my card!<br/>
-    Well, that's to say. It's your card now.<br/><br/>
-    What would you say if I told you it is one of a kind?<br/><br/>
-    Because it is.<br/>
+    I'm <b>David Vandenbogaerde</b>, or <i><a href="https://www.d17e.dev" target="_blank">d17e</a></i> for short. A software engineer and
+    budding
+    artist residing in Amsterdam.<br/><br/>
+    <b>Thank you</b> for scanning <i>my</i> card!<br/>
+    Well, that's to say... It's <i>your</i> card now.<br/><br/>
+    What would you say if I told you it is <b>one of a kind?</b><br/><br/>
+    Because <i>it is</i>.<br/>
     And I bet it looks just like this:
   </p>
 
@@ -31,9 +34,12 @@
 
   <p>
     <b>Why did I make these?</b><br/><br/>
-    Well, this is the kind of stuff I love doing and I believe it is a great way of showcasing what I can do.
-    Which is coming up with an idea and then making it happen. Turning ideas into art with code. And back again.<br/><br/>
-    I hope you like it! <br/>And if you do, <a
+    Well, this is the kind of stuff I 🧡 doing and I thought this is a great way of showcasing what I <i>can</i> do.
+    Which is, coming up with ideas and then making them happen. Start to finish.<br/>
+    Turning <b>ideas</b> into <b>art</b> through <b>code</b>.<br/>
+    And then back again.<br/><br/>
+    I hope you like it! <br/><br/>
+    And if you do, <a
           href="https://forms.d17e.dev/contact" target="_blank">let me know</a> and let's have a chat!
   </p>
 
